5be6b6ca233fe64c457d9eef6d7c171cd299399a
someshfengde <someshfengde@gmail.com>
2022-06-09 16:38:18 +0530

    forced_auth_obtaining and updating firmware

--- new_file.py
+++ new_file.py
@@ -2,26 +2,43 @@
 from pssh.clients import ParallelSSHClient
 hosts = ["172.31.255.155", "172.31.255.125"]
 
-client = ParallelSSHClient(hosts, user = "root" , password = "openwifi")
-#%%
-output = client.run_command("uname")
-for host_out in output:
-    for line in host_out.stdout:
-        print(line)
+def print_output(output):
+    for host_out in output:
+        for line in host_out.stdout:
+            print(line)
 
-print("----------")
+    print("----------")
+    
+def  command_to_aps(ip_list,user = "root" , password ="openwifi" ):
+    client = ParallelSSHClient(ip_list, user = user , password = password)
+    output = client.run_command("uname")
+    print_output(output)
+    output = client.run_command("ifconfig")
+    print_output(output)
+    dp = client.run_command("wget https://ucentral-ap-firmware.s3.amazonaws.com/20220202-indio_um-305ac-v2.4.1-6d9d4ab-upgrade.bin")
+    print(dp)
+    print("successful")
+    
+
+# client = ParallelSSHClient(hosts, user = "root" , password = "openwifi")
+
+
+# #%%
+# output = client.run_command("uname")
+
+
 
 # %%
-op = client.run_command("ifconfig")
+# op = client.run_command("ifconfig")
 
-# %%
-for host_out in op:
-    for line in host_out.stdout:
-        print(line)
-    print("--------------------------")
-# %%
+# # %%
+# for host_out in op:
+#     for line in host_out.stdout:
+#         print(line)
+#     print("--------------------------")
+# # %%
 
-dp = client.run_command("wget https://ucentral-ap-firmware.s3.amazonaws.com/20220202-indio_um-305ac-v2.4.1-6d9d4ab-upgrade.bin")
+# dp = client.run_command("wget https://ucentral-ap-firmware.s3.amazonaws.com/20220202-indio_um-305ac-v2.4.1-6d9d4ab-upgrade.bin")
 
 # for host_out in dp:
 #     for line in host_out.stdout:
---
+++ obtaining_forced_auth.py
@@ -0,0 +1,34 @@
+#%% 
+from ipaddress import ip_address
+import requests
+import pandas as pd 
+from new_file import * 
+
+
+mac_ids = ["72-6D-EC-0A-2E-B8","72-6D-EC-0A-27-48"]
+# we will get JSON data through API 
+# search mac_ids in JSON data 
+def ip_addresses_through_mac(mac_ids):
+    """
+        we will get JSON data through API 
+    """
+    response = requests.get('http://172.31.254.1/unibox/tools/getForceAuthData.php?', verify=False)
+    JSON_data = response.json()
+    df = pd.DataFrame(JSON_data['data'])
+    ip_addresses= []
+    for x in mac_ids:
+        successful = False
+        try:
+            ip_addresses.append(df[df['mac'] == x ]['ip'].to_numpy()[0])
+            successful = True
+        except:
+            pass
+        if not successful:
+            print(f"MAC {x} is not authenticated! Authenticate and try")
+
+    return ip_addresses
+ip_address = ip_addresses_through_mac(mac_ids)
+# %%
+print(ip_address)
+#%%
+command_to_aps(ip_address)
